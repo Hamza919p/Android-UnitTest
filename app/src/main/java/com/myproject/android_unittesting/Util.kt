69ef7aeab33748d0efae47f3f0c330b08633615b
hamza <hamza.919p@gmail.com>
2024-04-26 16:41:51 +0200

    added general util file

---
+++ app/src/main/java/com/myproject/android_unittesting/Util.kt
@@ -0,0 +1,8 @@
+package com.myproject.android_unittesting
+
+import kotlinx.coroutines.delay
+
+class Util {
+
+
+}
--- app/src/test/java/com/myproject/android_unittesting/ExampleUnitTest.kt
+++ app/src/test/java/com/myproject/android_unittesting/ExampleUnitTest.kt
@@ -1,8 +1,12 @@
 package com.myproject.android_unittesting
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.After
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +14,21 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+
+    val util = Util()
+    @Before
+    fun start() {
+
     }
+
+    @Test
+    fun print_simple_suspend_fun() {
+
+    }
+
+    @After
+    fun destroy() {
+
+    }
+
 }
