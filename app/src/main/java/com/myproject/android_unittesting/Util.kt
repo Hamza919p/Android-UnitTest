bcad8a86d76eb6e8a3bbb3c341ae52ece67bf2b2
hamza <hamza.919p@gmail.com>
2024-04-26 16:53:37 +0200

    Completed code

--- app/src/main/java/com/myproject/android_unittesting/Util.kt
+++ app/src/main/java/com/myproject/android_unittesting/Util.kt
@@ -1,8 +1,18 @@
 package com.myproject.android_unittesting
 
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class Util {
 
+    @OptIn(DelicateCoroutinesApi::class)
+    fun launchCoroutineOnThreadPool() {
+        GlobalScope.launch(Dispatchers.Main) {
+            println("Hello world")
+        }
+    }
 
 }
--- app/src/test/java/com/myproject/android_unittesting/ExampleUnitTest.kt
+++ app/src/test/java/com/myproject/android_unittesting/ExampleUnitTest.kt
@@ -1,7 +1,11 @@
 package com.myproject.android_unittesting
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Test
 
@@ -13,22 +17,29 @@ import org.junit.Before
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@OptIn(ExperimentalCoroutinesApi::class)
 class ExampleUnitTest {
 
     val util = Util()
+
+
+    val standardTestDispatcher = StandardTestDispatcher()
+
     @Before
     fun start() {
-
+        //When code runs, it runs on some thread i.e MAIN, IO.
+        //Here we have no thread we make a standard dispatcher
+        Dispatchers.setMain(standardTestDispatcher)
     }
 
     @Test
     fun print_simple_suspend_fun() {
-
+        util.launchCoroutineOnThreadPool()
     }
 
     @After
     fun destroy() {
-
+        Dispatchers.resetMain()
     }
 
 }
